8474216a5dfc22d7133665a795385c5499a7068a
Eliran Shemesh <eliran@steps.me>
2022-06-14 15:41:19 +0300

    actions effects

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -8,12 +8,22 @@ class Action {
 }
 
 const Actions = [
-	"Eating Healthy",
-	"Eating Unhealthy",
-	"Sleep Well",
-	"Not Enough Sleep",
-	"Sex",
-	"Masturbating",
-	"Designing Apps",
-	"Visiting Facebook",
+	{ name: "Eating Healthy" },
+	{ name: "Eating Unhealthy" },
+	{
+		name: "Not Eating",
+		effects: {
+			temperature: -0.5,
+			sharpness: +1,
+			mentalEnergy: -0.5,
+			physicalEnergy: -10,
+			social: -0.5,
+		},
+	},
+	{ name: "Sleep Well" },
+	{ name: "Not Enough Sleep" },
+	{ name: "Sex" },
+	{ name: "Masturbating" },
+	{ name: "Designing Apps" },
+	{ name: "Visiting Facebook" },
 ];
